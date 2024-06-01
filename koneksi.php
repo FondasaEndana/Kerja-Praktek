75689d651d903b18ecfd554e013cc637c9e64eb4
FondasaEndana <121755299+FondasaEndana@users.noreply.github.com>
2024-06-01 19:45:16 +0700

    Update now

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $conn = mysqli_connect("localhost","root","1234","studi_kasus");
-
-    if (mysqli_connect_errno()){
-        echo "Failed to login" . mysqli_connect_error();
-        exit();
-    }
-?>
